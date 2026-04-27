53defe2c78f589a7058b4a24182bec054802137c
LuisIslas <al222310478@gmail.com>
2026-04-27 09:00:52 -0600

    Motos

--- client/client.ts
+++ client/client.ts
@@ -1,4 +1,4 @@
 console.log("Wallet:", pg.wallet.publicKey.toString());
-uno
+
 const balance = await pg.connection.getBalance(pg.wallet.publicKey);
 console.log("Balance:", balance / web3.LAMPORTS_PER_SOL, "SOL");
